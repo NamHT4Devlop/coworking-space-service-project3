c8edfb0d1bdda9d0deb4408145d1ddc4a32d94fd
NamHT4Devlop <Trungnam5513@gmail.com>
2024-12-15 22:37:36 +0700

    update config and rebuild

--- analytics/model.py
+++ analytics/model.py
@@ -1,20 +1,15 @@
-from sqlalchemy.orm import registry
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy import Column, Integer, String, TIMESTAMP
 
-mapper_registry = registry()
+Base = declarative_base()
 
-@mapper_registry.mapped
-class Token:
+class Token(Base):
     __tablename__ = 'tokens'
-
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
     token = Column(String(6), nullable=False)
     created_at = Column(TIMESTAMP, nullable=False, default=func.now())
-    used_at = Column(TIMESTAMP, nullable=True)
-
+    used_at = Column(TIMESTAMP)
     def __repr__(self):
-        return (f"<Token(id={self.id}, user_id={self.user_id}, "
-                f"token='{self.token}', created_at='{self.created_at}', "
-                f"used_at='{self.used_at}')>")
+        return f"<Token(id={self.id}, user_id={self.user_id}, token='{self.token}', created_at='{self.created_at}', used_at='{self.used_at}')>"
