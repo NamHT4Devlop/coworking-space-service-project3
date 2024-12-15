572b15f6ef8b8b9b12f621bd928606da8d83a23e
NamHT4Devlop <Trungnam5513@gmail.com>
2024-12-15 19:30:07 +0700

    update config and rebuild

--- analytics/model.py
+++ analytics/model.py
@@ -1,15 +1,16 @@
+from dataclasses import dataclass
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy import Column, Integer, String, TIMESTAMP
 
 Base = declarative_base()
 
+@dataclass
 class Token(Base):
     __tablename__ = 'tokens'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
-    token = Column(String(6), nullable=False)
-    created_at = Column(TIMESTAMP, nullable=False, default=func.now())
-    used_at = Column(TIMESTAMP)
-    def __repr__(self):
-        return f"<Token(id={self.id}, user_id={self.user_id}, token='{self.token}', created_at='{self.created_at}', used_at='{self.used_at}')>"
+
+    id: int = Column(Integer, primary_key=True)
+    user_id: int = Column(Integer, nullable=False)
+    token: str = Column(String(6), nullable=False)
+    created_at: str = Column(TIMESTAMP, nullable=False, default=func.now())
+    used_at: str = Column(TIMESTAMP, nullable=True)
